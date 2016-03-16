1abdbc43845488069aa25dab935c78ac57fe7ac8
Ammie Liu <18366150440@163.com>
2016-03-16 18:26:00 +0800

    add html js resourse

---
+++ src/main/webapp/WEB-INF/views/js/main.js
@@ -0,0 +1,42 @@
+var myScroll;
+	function loaded() {
+		myScroll = new iScroll('wrapper', { scrollbarClass: 'myScrollbar' });
+	}
+
+	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+	document.addEventListener('DOMContentLoaded', loaded, false);
+
+
+/*$.ajax({
+    url: 'http://192.168.8.102:8080/api/v3/city/test1',
+    dataType: 'JSONP',
+    type: 'GET',
+    success: function (data) {
+        console.log(data);
+        alert("aaaa");
+    }
+});*/
+
+
+
+$(function(){
+	get_cover_img();
+})
+
+
+function get_cover_img(){
+	list_url='http://192.168.8.102:8080/api/v3/city/test1';
+	$.get(list_url,{},function(rsp){
+		lister(rsp);
+	},'jsonp');
+}
+
+function lister(rsp){
+	console.log(rsp);
+	
+}
+ 
+
+    
+    
