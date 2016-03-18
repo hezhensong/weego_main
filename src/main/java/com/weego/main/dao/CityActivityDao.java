2a47daf13abf2217964516153acff1843a93e492
JiangDandan <jiangdandan1818@163.com>
2016-03-18 17:41:50 +0800

    20160318 jiangdandan activity

--- src/main/java/com/weego/main/dao/CityActivityDao.java
+++ src/main/java/com/weego/main/dao/CityActivityDao.java
@@ -1,8 +1,12 @@
 package com.weego.main.dao;
 
+import java.util.List;
+
+import org.bson.types.ObjectId;
 import org.mongojack.JacksonDBCollection;
 import org.springframework.stereotype.Repository;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.weego.main.model.CityActivity;
@@ -10,18 +14,25 @@ import com.weego.main.model.CityActivity;
 @Repository
 public class CityActivityDao {
 
-	private DB database = MongoConnectionFactory.getDatabase();
+    private DB database = MongoConnectionFactory.getDatabase();
 
-	public CityActivity getSpecifiedCity(String cityActivityId) {
-		DBCollection collection = database.getCollection("activity");
+    public CityActivity getSpecifiedCity(String cityActivityId) {
+        
+        DBCollection collection = database.getCollection("activity");
 
-		JacksonDBCollection<CityActivity, String> coll;
-		coll = JacksonDBCollection.wrap(collection, CityActivity.class, String.class);
+        JacksonDBCollection<CityActivity, String> coll;
+        coll = JacksonDBCollection.wrap(collection, CityActivity.class, String.class);
 
-		// CityActivity cityActivity = coll.findOneById(cityActivityId);
-		CityActivity cityActivity = coll.findOne();
+        System.out.println("Dao层开始获取cityActivity");
+        BasicDBObject query = new BasicDBObject();
+        query.put("_id", new ObjectId(cityActivityId));
+        List<CityActivity> CityActivityList = coll.find(query).toArray();
 
-		return cityActivity;
+        if (CityActivityList.size() > 0) {
+            return CityActivityList.get(0);
+        } else {
+            return null;
+        }
 
-	}
+    }
 }
