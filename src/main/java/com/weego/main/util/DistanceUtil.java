acae5205f8c03bcd01afda6dca666faf21563365
CNUlichao <lichao_cnu@163.com>
2016-03-21 15:04:10 +0800

    distance bug

--- src/main/java/com/weego/main/util/DistanceUtil.java
+++ src/main/java/com/weego/main/util/DistanceUtil.java
@@ -18,27 +18,17 @@ public class DistanceUtil {
      *            第二个坐标的经度
      * @return 返回距离是 km
      */
-<<<<<<< HEAD
     public static double getDistance(String lat1, String lng1, String lat2, String lng2) {
-        Double Lat1 = Double.parseDouble(lat1);
-        Double Lng1 = Double.parseDouble(lng1);
-        Double Lat2 = Double.parseDouble(lat2);
-        Double Lng2 = Double.parseDouble(lng2);
-        double radLat1 = rad(Lat1);
-        double radLat2 = rad(Lat2);
-=======
-    public static double getDistance(String lat1, String lon1, String lat2, String lon2) {
         double radLat1 = rad(lat1);
         double radLat2 = rad(lat2);
->>>>>>> 7984c2be5df194dbf17bf6c97a3e4c4e07bcce19
         double a = radLat1 - radLat2;
-        double b = rad(Lng1) - rad(Lng2);
+        double b = rad(lng1) - rad(lng2);
         double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2)
                 * Math.pow(Math.sin(b / 2), 2)));
         s = s * EARTH_RADIUS;
         return s;
     }
-
+    
     public static void main(String[] args) {
 
         // 济南国际会展中心经纬度：117.11811 36.68484
@@ -52,11 +42,5 @@ public class DistanceUtil {
         System.out.println(getDistance("39.9960970000", "116.3640600000", "39.9672990000", "116.3650490000"));
 
         System.out.println(getDistance("36.68484", "117.11811", "36.66123", "117.00999000000002"));
-
-<<<<<<< HEAD
-        // long a= new Long("36.68484").longValue();
-        // System.out.println(a);
-=======
->>>>>>> 7984c2be5df194dbf17bf6c97a3e4c4e07bcce19
     }
 }
