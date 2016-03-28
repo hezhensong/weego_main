f394a3dfa75e6dc07a47912fdc5b3e2ed21a2e48
Liu Nian <liuniankachio@gmail.com>
2016-03-28 13:43:11 +0800

    新增c策略测试测试类

--- src/main/test/java/PolicyServiceTest.java
+++ src/main/test/java/PolicyServiceTest.java
@@ -17,7 +17,7 @@ public class PolicyServiceTest extends BaseTest{
     public void testPolicyService() {
         String cityId = "516a34f958e3511036000001";
         String time = "1457932600000";
-        List<Policy> policyList = policyService.fiterPolicyByTimeRange(cityId, time);
+        List<Policy> policyList = policyService.filterPolicyByTimeRange(cityId, time);
         System.out.println(policyList);
     }
 
@@ -25,7 +25,7 @@ public class PolicyServiceTest extends BaseTest{
     public void testPolicyServiceFilter() {
         String cityId = "516a34f958e3511036000001";
         String time = "1457932600000";
-        List<Policy> policyList = policyService.fiterPolicyByTimeRange(cityId, time);
+        List<Policy> policyList = policyService.filterPolicyByTimeRange(cityId, time);
         List<Policy> policyList1 = policyService.filterPolicyByDistance(policyList, "-72.963124,40.81184");
     }
 }
