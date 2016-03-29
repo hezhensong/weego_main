e24f2e900db0102fa395c2d8ad598efc0eb724f8
Liu Nian <liuniankachio@gmail.com>
2016-03-29 16:28:05 +0800

    feat: 修改动态推荐卡片新增坐标数据

--- src/main/java/com/weego/main/dto/RecommendCardDto.java
+++ src/main/java/com/weego/main/dto/RecommendCardDto.java
@@ -1,89 +1,27 @@
 package com.weego.main.dto;
 
+import java.util.List;
+
 /**
- * Created by liuniandxx on 16-3-19.
+ * Created by liuniandxx on 16-3-29.
  */
 public class RecommendCardDto {
-    private String id;
+    private String cityId;
+    private List<CardContentDto> content;
 
-    private Integer type;
-
-    private String firstTitle;
-
-    private String secondTitle;
-
-    private String title;
-
-    private String coverImage;
-
-    private String desc;
-
-    public String getId() {
-        return id;
+    public String getCityId() {
+        return cityId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
     }
 
-    public Integer getType() {
-        return type;
+    public List<CardContentDto> getContent() {
+        return content;
     }
 
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public String getFirstTitle() {
-        return firstTitle;
-    }
-
-    public void setFirstTitle(String firstTitle) {
-        this.firstTitle = firstTitle;
-    }
-
-    public String getSecondTitle() {
-        return secondTitle;
-    }
-
-    public void setSecondTitle(String secondTitle) {
-        this.secondTitle = secondTitle;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCoverImage() {
-        return coverImage;
-    }
-
-    public void setCoverImage(String coverImage) {
-        this.coverImage = coverImage;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public RecommendCardDto() {}
-
-    public RecommendCardDto(String id, int type, String firstTitle, String secondTitle,
-                            String coverImage, String title, String desc) {
-        this.id = id;
-        this.type = type;
-        this.firstTitle = firstTitle;
-        this.secondTitle = secondTitle;
-        this.coverImage = coverImage;
-        this.title = title;
-        this.desc = desc;
+    public void setContent(List<CardContentDto> content) {
+        this.content = content;
     }
 }
